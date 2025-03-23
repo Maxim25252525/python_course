4de5134e032d8bf186c551def1802a34269582cf
Maxim25252525 <149468309+Maxim25252525@users.noreply.github.com>
2025-03-23 16:55:29 +0300

    Changed code according to flake8

--- hw5/tests/hw5_task1_test.py
+++ hw5/tests/hw5_task1_test.py
@@ -9,7 +9,7 @@ def test_get_answer_without_marusya():
     assert get_answer("feffegовва") == "..."
 
 
-# строка содержит слово "Маруся",
-# но 'a', 'p', 'y', 'с' в обоих регистрах или 'M' могут быть написаны не по-русски
+# строка содержит слово "Маруся", но
+# 'a', 'p', 'y', 'с' в обоих регистрах или 'M' могут быть написаны не по-русски
 def test_get_answer_with_marusya_in_english():
     assert get_answer("Мaруcя") == "..."
